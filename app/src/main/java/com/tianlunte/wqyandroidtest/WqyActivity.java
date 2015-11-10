5fe8b3eeeb2d8b8748ac0d0079c31fbc80fea653
wangqingyun <caojiqueuer@gmail.com>
2015-11-10 09:41:59 +0800

    base

---
+++ app/src/main/java/com/tianlunte/wqyandroidtest/WqyActivity.java
@@ -0,0 +1,18 @@
+package com.tianlunte.wqyandroidtest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wangqingyun on 10/11/15.
+ */
+public class WqyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wqy);
+    }
+
+}
